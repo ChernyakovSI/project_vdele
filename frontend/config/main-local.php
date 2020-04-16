49b286078d7e808f29f5dadd404a66f70a1c1aae
ChernyakovSI <paladin_cool@inbox.ru>
2020-04-16 18:44:34 +0300

    - Настройки почтового сервиса. Отключен ssl

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,7 +3,7 @@ $config =  [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'ienQ5XySeNySTxI_PXEZUJxJ-EFHqSdz',
+            'cookieValidationKey' => 'OCYuWffeT9GjpgiAT2Bc0ITcNGyzPkb7',
         ],
     ],
 ];
