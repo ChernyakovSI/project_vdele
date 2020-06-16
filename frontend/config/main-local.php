1b76c150347af5945955029e27559e4866d04a4e
ChernyakovSI <paladin_cool@inbox.ru>
2020-06-16 20:00:05 +0300

    - Изменен механизм отправки почты

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,7 +3,7 @@ $config =  [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'uC6_3QYKAW9AKksx5mJfnfamwGlqDGnt',
+            'cookieValidationKey' => 'eKTmTfGkiCvBnNJtMD_NZMfBUC_2t7Qu',
         ],
     ],
 ];
