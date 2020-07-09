8df82b167e132612c11a51f18cda3efc91fecf61
ChernyakovSI <paladin_cool@inbox.ru>
2020-07-09 21:12:14 +0300

    - продакшн режим

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,12 +1,12 @@
 <?php
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            //'dsn' => 'mysql:host=localhost;dbname=gb_tasktracker',
-            'dsn' => 'mysql:host=localhost;dbname=che_goal',
-            'username' => 'root',
-            'password' => '431107',
+            'dsn' => 'mysql:host=sql213.main-hosting.eu;dbname=u237454327_vdele',
+            'username' => 'u237454327_palad',
+            'password' => '431107668727cnfc',
             'charset' => 'utf8',
         ],
         'mailer' => [
@@ -20,34 +20,31 @@ return [
                 'username' => 'robot@yavdele.net',
                 'password' => 'XX|jQ1XC$hf@wetl',
                 'port' => '587', // Port 25 is a very common port too
-                'encryption' => 'ssl', // It is often used, check your provider or mail server specs
+                //'encryption' => 'ssl', // It is often used, check your provider or mail server specs
             ],*/
         ],
 
-        /*'mailer' => [
+    ],
+];
+
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            //'dsn' => 'mysql:host=localhost;dbname=gb_tasktracker',
+            'dsn' => 'mysql:host=localhost;dbname=che_goal',
+            'username' => 'root',
+            'password' => '431107',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            //'useFileTransport' => true,
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.mail.ru',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'yavdele@list.ru',
-                'password' => '431107668727cnfc7',
-                'port' => '465', // Port 25 is a very common port too
-                //'encryption' => 'ssl', // It is often used, check your provider or mail server specs
-            ],
-            /*'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.hostinger.ru',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'robot@yavdele.net',
-                'password' => '431107668727cnfc7',
-                'port' => '587', // Port 25 is a very common port too
-                'encryption' => 'ssl', // It is often used, check your provider or mail server specs
-            ],*/
-        /*],*/
+            'useFileTransport' => true,
+        ],
     ],
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,10 @@
 <?php
+//Можно обращаться к ним Yii::$app->params['doman']
 return [
     'adminEmail' => 'yavdele@list.ru',
     'supportEmail' => 'yavdele@list.ru',
-    'robotEmail' => 'yavdele@list.ru',
+    'robotEmail' => 'robot@yavdele.net',
     'user.passwordResetTokenExpire' => 3600,
-    'doman' => 'http://yavdele.local/',
+    'doman' => 'http://yavdele.net/',
 ];
 
