58bb24cf15b4bd2f55a9c8c3116cc0d281ddf8ca
ChernyakovSI <paladin_cool@inbox.ru>
2020-07-16 20:12:58 +0300

    - Настройки боевоего сайта

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,33 +1,11 @@
 <?php
-
-$config = [
+$config =  [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'QAANdP88NcmIpD09obWDkISwkMoCHQjG',
+            'cookieValidationKey' => 'lOSl17Vc6E39Gop3rEB0FDJ0GxPEs8LT',
         ],
     ],
-
-    'controllerMap' => [
-        'ac' => 'frontend\controllers\SiteController',
-        //'yyy' => [
-        //    'class' => 'app\controllers\SiteController',
-        //    'enableCsrfValidation' => false,
-        //],
-    ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
 
 return $config;
