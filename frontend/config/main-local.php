c160a2b3df304139249740094e17bd781bcdcc8d
ChernyakovSI <paladin_cool@inbox.ru>
2020-07-19 19:38:25 +0300

    - Корректные настройки ++

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,7 +3,7 @@ $config =  [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Uy1AWZ8ioA14iLdTVtn_9yd3q9SZb46P',
+            'cookieValidationKey' => 'fkwV5OQRcWjTKqXqhUj8qJUDbMrE5ZJa',
         ],
     ],
 ];
