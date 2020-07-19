1dc760302ca8a2e52140c9b154a51faf053277bb
ChernyakovSI <paladin_cool@inbox.ru>
2020-07-19 19:36:14 +0300

    - Корректные настройки

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,33 +1,11 @@
 <?php
-
-$config = [
+$config =  [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'IKoWHzT4Q4U-NsIIWGVCihAqok8sU11u',
+            'cookieValidationKey' => 'Uy1AWZ8ioA14iLdTVtn_9yd3q9SZb46P',
         ],
     ],
-
-    'controllerMap' => [
-        'ac' => 'frontend\controllers\SiteController',
-        //'yyy' => [
-        //    'class' => 'app\controllers\SiteController',
-        //    'enableCsrfValidation' => false,
-        //],
-    ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
 
 return $config;
