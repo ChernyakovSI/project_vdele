a1325ee26fb7a25c18935164c9c17b6c40378530
ChernyakovSI <paladin_cool@inbox.ru>
2020-10-18 19:57:22 +0300

    - Разработаны диалоги

--- console/controllers/SendController.php
+++ console/controllers/SendController.php
@@ -25,10 +25,12 @@ class SendController extends Controller
     public function actionMail(){
         echo '111';
 
-        DialogUsers::renewSendedLettersAboutUnreadMessages();
-        $unreadDialogs = DialogUsers::getArrayOfUsersWithUnreadDialogs();
+        //DialogUsers::renewSendedLettersAboutUnreadMessages();
+        //$unreadDialogs = DialogUsers::getArrayOfUsersWithUnreadDialogs();
 
         echo '222';
+
+        exit();
 
         /*foreach ($unreadDialogs as $DialogUser){
             $anotherUser = DialogUsers::getAnotherUserInDialog($DialogUser['id_dialog'], $DialogUser['id_user']);
