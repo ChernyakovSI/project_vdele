ae96e988891dc04b08069e9c6c24f01188cb7b94
ChernyakovSI <paladin_cool@inbox.ru>
2020-10-20 21:28:11 +0300

    - Разработаны диалоги Авторассылка писем о непрочитанных сообщениях всем пользователям

--- console/controllers/SendController.php
+++ console/controllers/SendController.php
@@ -23,12 +23,8 @@ class SendController extends Controller
     }
 
     public function actionMail(){
-        echo '111';
-
         DialogUsers::renewSendedLettersAboutUnreadMessages();
         $unreadDialogs = DialogUsers::getArrayOfUsersWithUnreadDialogs();
-
-        //exit();
 
         foreach ($unreadDialogs as $DialogUser){
             $anotherUser = DialogUsers::getAnotherUserInDialog($DialogUser['id_dialog'], $DialogUser['id_user']);
@@ -36,6 +32,7 @@ class SendController extends Controller
 
 
             $userSender = User::findOne($anotherUser['id_user']);
+            $userReceiver = User::findOne($DialogUser['id_user']);
 
             $home_url = Yii::$app->params['doman'];
             //Формирование ссылки на страницу поста
@@ -53,8 +50,7 @@ class SendController extends Controller
             $subject = "Новое сообщение от ".$userSender->getFIO($anotherUser['id_user']);
 
 
-            Mailer::sendLetter($subject, $msg_html, 'paladin_cool@inbox.ru');
-            echo $full_link;
+            Mailer::sendLetter($subject, $msg_html, $userReceiver->getEmail());
 
             $dialog = DialogUsers::findOne($DialogUser['id']);
             $dialog->setSended(2);
