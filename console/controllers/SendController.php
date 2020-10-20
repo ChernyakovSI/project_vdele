cdc473d99b5579b4bf57870b42fc7076c74f997c
ChernyakovSI <paladin_cool@inbox.ru>
2020-10-20 21:32:46 +0300

    - Разработаны диалоги Автоотправка писем всем пользователям о непрочитанных сообщениях

--- console/controllers/SendController.php
+++ console/controllers/SendController.php
@@ -39,7 +39,7 @@ class SendController extends Controller
 
             echo $full_link;
 
-            $msg = "Здравствуйте! У вас есть непрочитанные сообщения от пользователя ".$userSender->getFIO($anotherUser['id_user']).". Перейдите по ссылке, чтобы прочитать сообщение: ".$full_link;
+            //$msg = "Здравствуйте! У вас есть непрочитанные сообщения от пользователя ".$userSender->getFIO($anotherUser['id_user']).". Перейдите по ссылке, чтобы прочитать сообщение: ".$full_link;
             $msg_html  = "<html><body style='font-family:Arial,sans-serif;'>";
             $msg_html .= "<h3 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Здравствуйте! У вас есть непрочитанные сообщения от пользователя  " . $userSender->getFIO($anotherUser['id_user']) . ".</h3>\r\n";
             $msg_html .= "<p><strong>Перейдите по ссылке, чтобы прочитать сообщение: </strong><a href='". $full_link ."'>$full_link</a></p>\r\n";
