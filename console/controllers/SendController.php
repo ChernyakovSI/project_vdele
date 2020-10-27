054fbbd7cf94b3f09f673156c9b7b9b7afb21c98
ChernyakovSI <paladin_cool@inbox.ru>
2020-10-27 15:04:23 +0300

    - Скорректированы письма о непрочитанных сообщениях

--- console/controllers/SendController.php
+++ console/controllers/SendController.php
@@ -40,10 +40,10 @@ class SendController extends Controller
             //$msg = "Здравствуйте! У вас есть непрочитанные сообщения от пользователя ".$userSender->getFIO($anotherUser['id_user']).". Перейдите по ссылке, чтобы прочитать сообщение: ".$full_link;
             $msg_html  = "<html><body style='font-family:Arial,sans-serif;'>";
             $msg_html .= "<h3 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Здравствуйте! У вас есть непрочитанные сообщения от пользователя  " . $userSender->getFI($anotherUser['id_user']) . ".</h3>\r\n";
-            $msg_html .= "<p><strong><a href='". $full_link ."'>Перейдите по ссылке</a>, чтобы прочитать сообщение: </strong></p>\r\n";
+            $msg_html .= "<p><strong><a href='". $full_link ."'>Перейдите по ссылке</a>, чтобы прочитать сообщение.</strong></p>\r\n";
             $msg_html .= "</body></html>";
 
-            $subject = "Новое сообщение от ".$userSender->getFIO($anotherUser['id_user']);
+            $subject = "Новое сообщение от ".$userSender->getFI($anotherUser['id_user']);
 
             Mailer::sendLetter($subject, $msg_html, $userReceiver->getEmail());
 
